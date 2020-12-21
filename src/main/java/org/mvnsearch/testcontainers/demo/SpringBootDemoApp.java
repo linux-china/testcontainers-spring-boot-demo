d0f609acb10058e57eba147fc223994f5a8ef4c7
linux_china <libing.chen@gmail.com>
2020-12-20 22:01:28 -0800

    SpringBootDemoApp

---
+++ src/main/java/org/mvnsearch/testcontainers/demo/SpringBootDemoApp.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.testcontainers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApp.class, args);
+	}
+}
