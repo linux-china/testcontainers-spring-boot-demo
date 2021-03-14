6c339d77056b6d8c7ee234e19e8ff0a41535d1ef
linux_china <libing.chen@gmail.com>
2021-03-14 12:51:58 -0700

    remove containers

--- src/test/java/org/mvnsearch/testcontainers/demo/SpringBootBaseTest.java
+++ src/test/java/org/mvnsearch/testcontainers/demo/SpringBootBaseTest.java
@@ -1,10 +1,6 @@
 package org.mvnsearch.testcontainers.demo;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Spring Boot base test case
@@ -12,16 +8,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
  * @author linux_china
  */
 @SpringBootTest
-@Testcontainers
 public abstract class SpringBootBaseTest {
-
-    @Container
-    public static RedisContainer redis = new RedisContainer();
-
-    @DynamicPropertySource
-    public static void redisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.redis.host", redis::getContainerIpAddress);
-        registry.add("spring.redis.port", () -> redis.getMappedPort(6379));
-    }
 
 }
