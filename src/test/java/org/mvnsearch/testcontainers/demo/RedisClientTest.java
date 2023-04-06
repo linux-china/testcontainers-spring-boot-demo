7de742c436ede373a84b84d71d110c6d2d9c7ebd
Libing Chen <libing.chen@gmail.com>
2023-04-06 12:22:42 +0800

    test: RedisClientTest

---
+++ src/test/java/org/mvnsearch/testcontainers/demo/RedisClientTest.java
@@ -0,0 +1,21 @@
+package org.mvnsearch.testcontainers.demo;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import redis.clients.jedis.Jedis;
+
+
+@Testcontainers
+public class RedisClientTest {
+
+    @Container
+    static RedisContainer redisContainer = new RedisContainer();
+
+    @Test
+    public void testOperations() {
+        Jedis jedis = redisContainer.getJedis();
+        jedis.set("nick", "leijuan");
+        System.out.println(jedis.get("nick"));
+    }
+}
