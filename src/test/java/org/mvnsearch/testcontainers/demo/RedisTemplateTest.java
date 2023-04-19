d63815791275cded4fc2ab95146654260e55a034
Libing Chen <libing.chen@gmail.com>
2023-04-19 15:16:52 +0800

    test: extends TestContainersBaseTest

--- src/test/java/org/mvnsearch/testcontainers/demo/RedisTemplateTest.java
+++ src/test/java/org/mvnsearch/testcontainers/demo/RedisTemplateTest.java
@@ -2,6 +2,7 @@ package org.mvnsearch.testcontainers.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
 /**
@@ -9,14 +10,15 @@ import org.springframework.data.redis.core.RedisTemplate;
  *
  * @author linux_china
  */
-public class RedisTemplateTest extends SpringBootBaseTest {
+@SpringBootTest
+public class RedisTemplateTest extends TestContainersBaseTest {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
     @Test
     public void testOperation() {
-        redisTemplate.opsForValue().set("nick", "leijuan");
+        redisTemplate.opsForValue().set("nick", "leijuan33");
         System.out.println(redisTemplate.opsForValue().get("nick"));
     }
 }
