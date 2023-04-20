84d2d2bbf1595fded46b0c6196c2a014162b6198
Libing Chen <libing.chen@gmail.com>
2023-04-20 15:57:08 +0800

    feat: add MySQLContainer

--- src/test/java/org/mvnsearch/testcontainers/demo/TestContainersConfiguration.java
+++ src/test/java/org/mvnsearch/testcontainers/demo/TestContainersConfiguration.java
@@ -3,6 +3,7 @@ package org.mvnsearch.testcontainers.demo;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration
 public class TestContainersConfiguration {
@@ -11,5 +12,11 @@ public class TestContainersConfiguration {
     @ServiceConnection
     public RedisContainer redisContainer() {
         return new RedisContainer();
+    }
+
+    @Bean
+    @ServiceConnection
+    public MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>("mysql:8.0.23");
     }
 }
