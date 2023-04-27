f606e7d1fd138ea616baf5a0cc66a1550c0004bb
Libing Chen <libing.chen@gmail.com>
2023-04-27 22:34:03 +0800

    feat: add mailhog

--- src/test/java/org/mvnsearch/testcontainers/demo/TestContainersConfiguration.java
+++ src/test/java/org/mvnsearch/testcontainers/demo/TestContainersConfiguration.java
@@ -3,10 +3,26 @@ package org.mvnsearch.testcontainers.demo;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 
-@TestConfiguration
+@SuppressWarnings("resource")
+@TestConfiguration(proxyBeanMethods = false)
 public class TestContainersConfiguration {
+
+    static GenericContainer<?> mailHog = new GenericContainer<>("mailhog/mailhog:latest")
+            .withExposedPorts(1025, 8025);
+
+    static {
+        mailHog.start();
+        System.setProperty("spring.mail.host", mailHog.getHost());
+        System.setProperty("spring.mail.port", mailHog.getMappedPort(1025).toString());
+    }
+
+    @Bean
+    public GenericContainer<?> mailHog() {
+        return mailHog;
+    }
 
     @Bean
     @ServiceConnection
