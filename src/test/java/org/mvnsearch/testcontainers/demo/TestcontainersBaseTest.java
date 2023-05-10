1ab000f8062352a48f83abd2157174c2377774ad
Libing Chen <libing.chen@gmail.com>
2023-05-10 17:09:19 +0800

    test: add @DBRider

--- src/test/java/org/mvnsearch/testcontainers/demo/TestcontainersBaseTest.java
+++ src/test/java/org/mvnsearch/testcontainers/demo/TestcontainersBaseTest.java
@@ -1,5 +1,8 @@
 package org.mvnsearch.testcontainers.demo;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.configuration.Orthography;
+import com.github.database.rider.spring.api.DBRider;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -10,6 +13,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
  */
 @Import(TestContainersConfiguration.class)
 @Testcontainers
+@DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE, columnSensing = true)
 public abstract class TestcontainersBaseTest extends SpringBootBaseTest {
 
 }
