97534134222497434cb2c4769c259f9e8b7b6b9c
Libing Chen <libing.chen@gmail.com>
2024-11-23 12:37:11 +0800

    test: use DynamicPropertyRegistrar introduced with spring boot 3.4

--- src/test/java/org/mvnsearch/testcontainers/demo/TestContainersConfiguration.java
+++ src/test/java/org/mvnsearch/testcontainers/demo/TestContainersConfiguration.java
@@ -4,21 +4,31 @@ import org.mvnsearch.testcontainers.demo.redis.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SuppressWarnings("resource")
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainersConfiguration {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Bean()
-    public GenericContainer<?> mailpit(DynamicPropertyRegistry properties) {
-        GenericContainer<?> mailpit = new GenericContainer<>("axllent/mailpit:v1.20.4").withExposedPorts(1025, 8025);
-        properties.add("spring.mail.host", mailpit::getHost);
-        properties.add("spring.mail.port", () -> mailpit.getMappedPort(1025).toString());
-        return mailpit;
+    @Container
+    static GenericContainer<?> mailpit = new GenericContainer<>("axllent/mailpit:v1.20.4").withExposedPorts(1025, 8025);
+    @Container
+    static GenericContainer<?> natsServer = new GenericContainer<>("nats:2.10.22-alpine").withExposedPorts(4222);
+
+    @Bean
+    public DynamicPropertyRegistrar dynamicPropertyRegistrar() {
+        return registry -> {
+            registry.add("spring.mail.host", mailpit::getHost);
+            registry.add("spring.mail.port", () -> mailpit.getMappedPort(1025).toString());
+            registry.add("nats.spring.server", () -> {
+                String host = natsServer.getHost();
+                Integer port = natsServer.getMappedPort(4222);
+                return String.format("nats://%s:%d", host, port);
+            });
+        };
     }
 
     @Bean
