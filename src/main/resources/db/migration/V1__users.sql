92d39b01a85161f3e74e37c230643dce16665735
Libing Chen <libing.chen@gmail.com>
2025-05-29 10:37:08 +0800

    db: polishing

--- src/main/resources/db/migration/V1__users.sql
+++ src/main/resources/db/migration/V1__users.sql
@@ -1,9 +1,8 @@
 CREATE TABLE users
 (
-    id SERIAL,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp NOT NULL,
-    CONSTRAINT pk_user PRIMARY KEY (id)
+    id         SERIAL primary key,
+    name       varchar(255) NOT NULL,
+    email      varchar(255) NOT NULL,
+    created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp    NOT NULL
 );
